4943b4863e40450b034789e3bae83788df39d4fa
lucerion <alexander.skachko@gmail.com>
2020-09-21 16:27:15 +0300

    Add recompile command to iex config

--- .iex.exs
+++ .iex.exs
@@ -4,5 +4,14 @@ IEx.configure(history_size: 4096)
 
 defmodule :iex do
   def exit,   do: System.stop()
+
   def reload, do: respawn()
+
+  def recompile do
+    app = Mix.Project.config[:app]
+    Mix.Task.reenable "compile.elixir"
+    Application.stop(app)
+    Mix.Task.run "compile.elixir"
+    Application.start(app, :permanent)
+  end
 end
