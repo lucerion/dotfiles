b9a870877ffa2c852cbe91b01432d1a27269a732
lucerion <alexander.skachko@gmail.com>
2022-11-22 16:35:16 +0300

    Cleanup .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,17 +1,12 @@
 import_if_available Ecto.Query
 
-IEx.configure(history_size: 4096)
+IEx.configure(
+  history_size: 4096,
+  inspect: [limit: :infinity]
+)
 
 defmodule :iex do
-  def exit,   do: System.stop()
+  def exit, do: System.stop()
 
   def reload, do: respawn()
-
-  def recompile do
-    app = Mix.Project.config[:app]
-    Mix.Task.reenable("compile.elixir")
-    Application.stop(app)
-    Mix.Task.run("compile.elixir")
-    Application.start(app, :permanent)
-  end
 end
